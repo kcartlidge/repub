317c9b41d0880c3b21a5e63b833aaa0a66af3861
K Cartlidge <code@kcartlidge.com>
2026-08-31 22:18:36 +0100

    Extract EPUB archives to a temp folder

--- src/read-epub.js
+++ src/read-epub.js
@@ -1,7 +1,22 @@
 'use strict';
 
-async function readEpub(epubPath) {
-  throw new Error('readEpub not implemented');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const unzipper = require('unzipper');
+
+async function extractEpub(epubPath) {
+  const resolved = path.resolve(epubPath);
+  await fs.promises.access(resolved, fs.constants.R_OK);
+
+  const extractDir = await fs.promises.mkdtemp(path.join(os.tmpdir(), 'repub-'));
+  await fs.createReadStream(resolved).pipe(unzipper.Extract({ path: extractDir })).promise();
+  return extractDir;
 }
 
-module.exports = { readEpub };
+async function readEpub(epubPath) {
+  const extractDir = await extractEpub(epubPath);
+  return { extractDir };
+}
+
+module.exports = { extractEpub, readEpub };
